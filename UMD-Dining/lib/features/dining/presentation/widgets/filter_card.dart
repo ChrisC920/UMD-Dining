3fda78639bd1cb699368846db35d7dad11ff824e
Christopher Cho <christophercho0916@gmail.com>
2025-04-10 13:06:53 -0400

    filter by date

--- UMD-Dining/lib/features/dining/presentation/widgets/filter_card.dart
+++ UMD-Dining/lib/features/dining/presentation/widgets/filter_card.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class FilterCard extends StatefulWidget {
   final Set<String> selectedAllergens;
   final Set<String> selectedMealTypes;
   final Set<String> selectedDietaryPreferences;
-  final void Function(Set<String> allergens, Set<String> mealTypes, Set<String> dietaryPreferences) onApply;
+  final DateTime? selectedDate;
+  final void Function(Set<String> allergens, Set<String> mealTypes, Set<String> dietaryPreferences, DateTime? selectedDate) onApply;
 
   const FilterCard({
     super.key,
     required this.selectedAllergens,
     required this.selectedMealTypes,
     required this.selectedDietaryPreferences,
+    required this.selectedDate,
     required this.onApply,
   });
 
@@ -19,6 +22,7 @@ class FilterCard extends StatefulWidget {
 }
 
 class _FilterCardState extends State<FilterCard> {
+  late DateTime? selectedDate;
   late Set<String> selectedAllergens;
   late Set<String> selectedMealTypes;
   late Set<String> selectedDietaryPreferences;
@@ -51,6 +55,7 @@ class _FilterCardState extends State<FilterCard> {
   @override
   void initState() {
     super.initState();
+    selectedDate = widget.selectedDate;
     selectedAllergens = Set.from(widget.selectedAllergens);
     selectedMealTypes = Set.from(widget.selectedMealTypes);
     selectedDietaryPreferences = Set.from(widget.selectedDietaryPreferences);
@@ -92,7 +97,6 @@ class _FilterCardState extends State<FilterCard> {
                 ),
               ],
             ),
-            // const SizedBox(height: 16),
             const Divider(height: 30, thickness: 1),
 
             // Meal Types
@@ -113,27 +117,25 @@ class _FilterCardState extends State<FilterCard> {
                     style: TextStyle(
                       color: selected ? Colors.black : Colors.white,
                       fontWeight: FontWeight.w500,
+                      fontSize: 16,
                     ),
                   ),
                   selected: selected,
                   selectedColor: Colors.white, // Color when selected
-                  backgroundColor: Colors.grey[900], // Color when not selected
                   onSelected: (_) {
                     setState(() {
                       selected ? selectedMealTypes.remove(meal) : selectedMealTypes.add(meal);
-                      widget.onApply(selectedAllergens, selectedMealTypes, selectedDietaryPreferences);
+                      widget.onApply(selectedAllergens, selectedMealTypes, selectedDietaryPreferences, selectedDate);
                     });
                   },
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
-                    // side: BorderSide(color: selected ? Colors.red : Colors.grey.shade400),
                   ),
                   elevation: 2,
                 );
               }).toList(),
             ),
 
-            // const SizedBox(height: 24),
             const Divider(height: 30, thickness: 1),
             // Allergens
             Align(
@@ -144,16 +146,13 @@ class _FilterCardState extends State<FilterCard> {
               ),
             ),
 
-            const SizedBox(height: 8),
-
             Wrap(
               spacing: 8,
-              // runSpacing: 0,
               children: dietaryPreferences.map((dietaryPreference) {
                 return SizedBox(
                   width: MediaQuery.of(context).size.width / 2 - 32, // half width minus padding
                   child: CheckboxListTile(
-                    title: Text(dietaryPreference),
+                    title: Text(dietaryPreference, style: Theme.of(context).textTheme.titleMedium),
                     value: selectedDietaryPreferences.contains(dietaryPreference),
                     onChanged: (bool? value) {
                       setState(() {
@@ -162,7 +161,7 @@ class _FilterCardState extends State<FilterCard> {
                         } else {
                           selectedDietaryPreferences.remove(dietaryPreference);
                         }
-                        widget.onApply(selectedAllergens, selectedMealTypes, selectedDietaryPreferences);
+                        widget.onApply(selectedAllergens, selectedMealTypes, selectedDietaryPreferences, selectedDate);
                       });
                     },
                     controlAffinity: ListTileControlAffinity.leading,
@@ -184,16 +183,13 @@ class _FilterCardState extends State<FilterCard> {
               ),
             ),
 
-            const SizedBox(height: 8),
-
             Wrap(
               spacing: 8,
-              // runSpacing: 0,
               children: allergens.map((allergen) {
                 return SizedBox(
                   width: MediaQuery.of(context).size.width / 2 - 32, // half width minus padding
                   child: CheckboxListTile(
-                    title: Text(allergen),
+                    title: Text(allergen, style: Theme.of(context).textTheme.titleMedium),
                     value: selectedAllergens.contains(allergen),
                     onChanged: (bool? value) {
                       setState(() {
@@ -202,7 +198,7 @@ class _FilterCardState extends State<FilterCard> {
                         } else {
                           selectedAllergens.remove(allergen);
                         }
-                        widget.onApply(selectedAllergens, selectedMealTypes, selectedDietaryPreferences);
+                        widget.onApply(selectedAllergens, selectedMealTypes, selectedDietaryPreferences, selectedDate);
                       });
                     },
                     controlAffinity: ListTileControlAffinity.leading,
@@ -215,6 +211,55 @@ class _FilterCardState extends State<FilterCard> {
             ),
             const Divider(height: 30, thickness: 1),
 
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Filter by Date",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  selectedDate != null ? DateFormat('EEEE, MMMM d').format(selectedDate!) : 'No date selected',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () async {
+                        final picked = await showDatePicker(
+                          context: context,
+                          initialDate: selectedDate,
+                          firstDate: DateTime(2025, 4, 3),
+                          lastDate: DateTime.now(),
+                        );
+                        if (picked != null) {
+                          setState(() {
+                            selectedDate = picked;
+                          });
+                        }
+                      },
+                      child: const Text("Choose Date", style: TextStyle(fontSize: 16)),
+                    ),
+                    if (selectedDate != null)
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            selectedDate = null;
+                          });
+                        },
+                        child: const Text("Clear", style: TextStyle(fontSize: 16)),
+                      ),
+                  ],
+                ),
+              ],
+            ),
+
+            const Divider(height: 30, thickness: 1),
+
             // Buttons
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -225,17 +270,21 @@ class _FilterCardState extends State<FilterCard> {
                       selectedAllergens.clear();
                       selectedMealTypes.clear();
                       selectedDietaryPreferences.clear();
-                      widget.onApply(selectedAllergens, selectedMealTypes, selectedDietaryPreferences);
+                      selectedDate = null;
+                      widget.onApply(selectedAllergens, selectedMealTypes, selectedDietaryPreferences, selectedDate);
                     });
                   },
-                  child: const Text('Clear'),
+                  child: const Text(
+                    'Clear',
+                    style: TextStyle(fontSize: 16),
+                  ),
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    widget.onApply(selectedAllergens, selectedMealTypes, selectedDietaryPreferences);
+                    widget.onApply(selectedAllergens, selectedMealTypes, selectedDietaryPreferences, selectedDate);
                     Navigator.pop(context);
                   },
-                  child: const Text('Apply'),
+                  child: const Text('Apply', style: TextStyle(fontSize: 16)),
                 ),
               ],
             )
